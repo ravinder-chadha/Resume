1490fa350291f9652567fbc2eb1c28891881c47e
Ravinder Chadha <ravinderchadha1233@gmail.com>
2021-09-11 15:50:44 +0530

    updated Resume

--- op.js
+++ op.js
@@ -1,30 +1,3 @@
-// $(document).keydown(function(e){ 
-//   if(e.which === 123){
-//      return false;
-//   }
-// }); 
-
-// $(document).bind("contextmenu",function(e) {  
-// 	e.preventDefault(); 
- 
-// });
-
-// var currentHtmlContent; 
-// var element = new Image(); 
-// var elementWithHiddenContent = document.querySelector("#element-to-hide"); 
-// var innerHtml = elementWithHiddenContent.innerHTML;
-// element.__defineGetter__("id", function() { 
-//     currentHtmlContent= ""; 
-// });
-
-
-setInterval(function() { 
-    currentHtmlContent= innerHtml; 
-    console.log(element); 
-    console.clear();  
-    elementWithHiddenContent.innerHTML = currentHtmlContent; 
-}, 1000); 
-
 //skills animation
 const val1 = document.querySelector('.skills');
 val1.classList.remove('skills-animation');
